8bcc613db135aa405a779e32f607f6c4bc9faea5
David M. Gailey <dmg197@psu.edu>
2019-10-31 12:02:20 -0400

    revert to audio to text google code sample, add times in

--- transcribe-speech-to-text.js
+++ transcribe-speech-to-text.js
@@ -2,64 +2,82 @@
 // https://cloud.google.com/speech-to-text/docs/quickstart-client-libraries
 
 
-// Imports the Google Cloud client library
-const speech = require('@google-cloud/speech');
-const fs = require('fs')
 
-// Creates a client
-const client = new speech.SpeechClient();
+async function main() {
+		// Imports the Google Cloud client library
+		const speech = require('@google-cloud/speech');
+		const fs = require('fs');
+	
+		// Creates a client
+		const client = new speech.SpeechClient();
+	
+		// The name of the audio file to transcribe
+		const fileName = './resources/IMG_3334_1.mp3';
+	
+		// Reads a local audio file and converts it to base64
+		const file = fs.readFileSync(fileName);
+		const audioBytes = file.toString('base64');
+	
+		// The audio file's encoding, sample rate in hertz, and BCP-47 language code
+		const audio = {
+			content: audioBytes,
+		};
+		const config = {
+			enableWordTimeOffsets: true,
+			encoding: 'MP3',
+			sampleRateHertz: 44100,
+			languageCode: 'en-US',
+		};
+		const request = {
+			audio: audio,
+			config: config,
+		};
+	
+		// Detects speech in the audio file
+		const [response] = await client.recognize(request);
+		const transcription = response.results
+			.map(result => result.alternatives[0].transcript)
+			.join('\n');
 
-// The name of the audio file to transcribe
-const fileName = './resources/audio.raw';
+		console.log(`\n\nTranscription (whole): ${transcription}`);
 
-// Reads a local audio file and converts it to base64
-const file = fs.readFileSync(fileName);
-const audioBytes = file.toString('base64');
+		console.log('\n------------------------------------ \n');
 
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
-const encoding = 'MP3';
-const sampleRateHertz = 44100;
-const languageCode = 'en-US';
+		response.results.forEach(result => {
 
-const config = {
-    enableWordTimeOffsets: true,
-    encoding: encoding,
-    sampleRateHertz: sampleRateHertz,
-    languageCode: languageCode,
-};
+			
 
-// The audio file's encoding, sample rate in hertz, and BCP-47 language code
-const audio = {
-    content: audioBytes,
-};
+			// get start time of first word
+			const wordInfo = result.alternatives[0].words[0]
+			const startSecs =
+				`${wordInfo.startTime.seconds}` +
+				`.` +
+				wordInfo.startTime.nanos / 100000000;
+			const endSecs =
+				`${wordInfo.endTime.seconds}` +
+				`.` +
+				wordInfo.endTime.nanos / 100000000;
 
-const request = {
-    config: config,
-    audio: audio,
-};
+			console.log(`\t ${startSecs} secs - ${endSecs} secs`);
+			console.log(`Transcription: ${result.alternatives[0].transcript}`);
+			
+			
 
-// Detects speech in the audio file. This creates a recognition job that you
-// can wait for now, or get its result later.
-const [operation] = await client.recognize(request);
-
-// Get a Promise representation of the final result of the job
-const [response] = await operation.promise();
-response.results.forEach(result => {
-    console.log(`Transcription: ${result.alternatives[0].transcript}`);
-    result.alternatives[0].words.forEach(wordInfo => {
-        // NOTE: If you have a time offset exceeding 2^32 seconds, use the
-        // wordInfo.{x}Time.seconds.high to calculate seconds.
-        const startSecs =
-            `${wordInfo.startTime.seconds}` +
-            `.` +
-            wordInfo.startTime.nanos / 100000000;
-        const endSecs =
-            `${wordInfo.endTime.seconds}` +
-            `.` +
-            wordInfo.endTime.nanos / 100000000;
-        console.log(`Word: ${wordInfo.word}`);
-        console.log(`\t ${startSecs} secs - ${endSecs} secs`);
-    });
-});
+			// loop through words in each transcript line
+			// result.alternatives[0].words.forEach(wordInfo => {
+			//   // NOTE: If you have a time offset exceeding 2^32 seconds, use the
+			//   // wordInfo.{x}Time.seconds.high to calculate seconds.
+			//   const startSecs =
+			// 	`${wordInfo.startTime.seconds}` +
+			// 	`.` +
+			// 	wordInfo.startTime.nanos / 100000000;
+			//   const endSecs =
+			// 	`${wordInfo.endTime.seconds}` +
+			// 	`.` +
+			// 	wordInfo.endTime.nanos / 100000000;
+			//   console.log(`Word: ${wordInfo.word}`);
+			//   console.log(`\t ${startSecs} secs - ${endSecs} secs`);
+			// });
+		  });
+	}
+	main().catch(console.error);
